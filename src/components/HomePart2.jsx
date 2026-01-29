9e0502ae46d85692dad4c5e7ba0408fe2485b0cc
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-01-29 15:40:29 +0530

    style using different methods

--- src/components/HomePart2.jsx
+++ src/components/HomePart2.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+const styles = {
+  heading: {
+    background: "linear-gradient(to top, #facc15, #f87171)", 
+    WebkitBackgroundClip: "text",
+    color: "transparent",
+    textAlign: "center",
+    fontSize: "1.875rem", 
+    fontWeight: "bold",
+  },
+  mainHeading: {
+    textAlign: "center",
+    color: "white",
+    fontSize: "3.75rem", 
+  },
+  subHeading: {
+    background: "linear-gradient(to top, #facc15, #f87171)", 
+    WebkitBackgroundClip: "text",
+    color: "transparent",
+    textAlign: "center",
+    fontSize: "3.75rem", 
+    fontWeight: "bold",
+  },
+  paragraphContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "1rem", 
+  },
+  paragraph: {
+    color: "#9ca3af", 
+    fontSize: "1.125rem", 
+    fontWeight: "600", 
+    width: "50%", 
+    textAlign: "center",
+  },
+};
 
 export const HomePart2 = () => {
   return (
     <div>
-      <h2 class="bg-linear-to-t from-yellow-400 to-red-600 bg-clip-text text-transparent text-center text-3xl font-bold">
-        Our Mission
-      </h2>
-      <h1 className="text-center  text-white text-6xl">
-        Lorem ipsum dolor sit amet
-      </h1>
-      <h1 className="text-center bg-linear-to-t from-yellow-400 to-red-600 bg-clip-text text-transparent font-bold text-6xl">
-        consectetur adipisicing
-      </h1>
-      <div className="flex justify-center items-center mt-4">
-        <p className="text-gray-400 text-lg font-semibold w-1/2 text-center">
+      <h2 style={styles.heading}>Our Mission</h2>
+      <h1 style={styles.mainHeading}>Lorem ipsum dolor sit amet</h1>
+      <h1 style={styles.subHeading}>consectetur adipisicing</h1>
+      <div style={styles.paragraphContainer}>
+        <p style={styles.paragraph}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolores,
           animi repudiandae exercitationem illo corporis excepturi aut ipsum
           aspernatur inventore dolor possimus facere, itaque praesentium iusto.
--- src/components/HomePart3.jsx
+++ src/components/HomePart3.jsx
@@ -1,39 +1,64 @@
-import React from "react";
+import styled from "styled-components";
 import cloud from "../assets/cloud.svg";
+
+const Container = styled.div`
+  display: flex;
+  margin: 1rem 2.5rem;  // mx-10 my-4
+  justify-content: space-around;
+`;
+
+const Card = styled.div`
+  margin: 0 1rem;         // mx-4
+  width: 25%;             // w-3/12
+  text-align: center;
+`;
+
+const Image = styled.img`
+  height: 5rem;           // h-20
+  width: 5rem;            // w-20
+`;
+
+const Heading = styled.h3`
+  color: white;
+  font-size: 1.25rem;     // text-xl
+  font-weight: bold;      // font-bold
+  margin-top: 1rem;       // mt-4
+`;
+
+const Paragraph = styled.p`
+  color: #9ca3af;         // text-gray-400
+  font-size: 0.875rem;    // text-sm
+  font-weight: 600;       // font-semibold
+  margin-top: 1rem;       // mt-4
+`;
 
 export const HomePart3 = () => {
   return (
-    <div className="flex mx-10 my-4 justify-around ">
-      <div className=" mx-4 w-3/12">
-        <img src={cloud} alt="" className="h-20 w-20" />
-        <h3 className="text-white text-xl font-bold mt-4">
-          Lorem ipsum dolor sit amet.
-        </h3>
-        <p className="text-gray-400 text-sm font-semibold mt-4">
+    <Container>
+      <Card>
+        <Image src={cloud} alt="Cloud" />
+        <Heading>Lorem ipsum dolor sit amet.</Heading>
+        <Paragraph>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas dolorem
           minima sequi sunt neque ipsum ipsam repudiandae, nisi quod itaque!
-        </p>
-      </div>
-      <div className=" mx-4 mt-20 w-3/12">
-        <img src={cloud} alt="" className="h-20 w-20" />
-        <h3 className="text-white text-xl font-bold mt-4">
-          Lorem ipsum dolor sit amet.
-        </h3>
-        <p className="text-gray-400 text-sm font-semibold mt-4">
+        </Paragraph>
+      </Card>
+      <Card style={{ marginTop: "5rem" }}>
+        <Image src={cloud} alt="Cloud" />
+        <Heading>Lorem ipsum dolor sit amet.</Heading>
+        <Paragraph>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas dolorem
           minima sequi sunt neque ipsum ipsam repudiandae, nisi quod itaque!
-        </p>
-      </div>
-      <div className=" mx-4 w-3/12 mt-40">
-        <img src={cloud} alt="" className="h-20 w-20" />
-        <h3 className="text-white text-xl font-bold mt-4">
-          Lorem ipsum dolor sit amet.
-        </h3>
-        <p className="text-gray-400 text-sm font-semibold mt-4">
+        </Paragraph>
+      </Card>
+      <Card style={{ marginTop: "10rem" }}>
+        <Image src={cloud} alt="Cloud" />
+        <Heading>Lorem ipsum dolor sit amet.</Heading>
+        <Paragraph>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas dolorem
           minima sequi sunt neque ipsum ipsam repudiandae, nisi quod itaque!
-        </p>
-      </div>
-    </div>
+        </Paragraph>
+      </Card>
+    </Container>
   );
 };
