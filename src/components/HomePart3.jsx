39d6d7fd225cd9270236c56013be5c17757d3ffa
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-01-30 14:46:22 +0530

    style:HomePart3 responsive

--- src/components/HomePart3.jsx
+++ src/components/HomePart3.jsx
@@ -3,33 +3,85 @@ import cloud from "../assets/cloud.svg";
 
 const Container = styled.div`
   display: flex;
-  margin: 1rem 2.5rem;  // mx-10 my-4
   justify-content: space-around;
+  margin: 1rem 2.5rem;
+  gap: 2rem;
+
+  @media (max-width: 1024px) {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  @media (max-width: 640px) {
+    flex-direction: column;
+    align-items: center;
+    margin: 1rem;
+  }
 `;
 
 const Card = styled.div`
-  margin: 0 1rem;         // mx-4
-  width: 25%;             // w-3/12
+  width: 25%;
   text-align: center;
+
+  &:nth-child(2) {
+    margin-top: 5rem;
+  }
+
+  &:nth-child(3) {
+    margin-top: 10rem;
+  }
+
+  @media (max-width: 1024px) {
+    width: 45%;
+
+    &:nth-child(2),
+    &:nth-child(3) {
+      margin-top: 3rem;
+    }
+  }
+
+  @media (max-width: 640px) {
+    width: 100%;
+    margin-top: 2rem;
+
+    &:nth-child(2),
+    &:nth-child(3) {
+      margin-top: 2rem;
+    }
+  }
 `;
 
 const Image = styled.img`
-  height: 5rem;           // h-20
-  width: 5rem;            // w-20
+  height: 5rem;
+  width: 5rem;
+
+  @media (max-width: 640px) {
+    height: 4rem;
+    width: 4rem;
+  }
 `;
 
 const Heading = styled.h3`
   color: white;
-  font-size: 1.25rem;     // text-xl
-  font-weight: bold;      // font-bold
-  margin-top: 1rem;       // mt-4
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-top: 1rem;
+
+  @media (max-width: 640px) {
+    font-size: 1.1rem;
+  }
 `;
 
 const Paragraph = styled.p`
-  color: #9ca3af;         // text-gray-400
-  font-size: 0.875rem;    // text-sm
-  font-weight: 600;       // font-semibold
-  margin-top: 1rem;       // mt-4
+  color: #9ca3af;
+  font-size: 0.875rem;
+  font-weight: 600;
+  margin-top: 1rem;
+  line-height: 1.6;
+
+  @media (max-width: 640px) {
+    font-size: 0.8rem;
+  }
 `;
 
 export const HomePart3 = () => {
@@ -43,7 +95,8 @@ export const HomePart3 = () => {
           minima sequi sunt neque ipsum ipsam repudiandae, nisi quod itaque!
         </Paragraph>
       </Card>
-      <Card style={{ marginTop: "5rem" }}>
+
+      <Card>
         <Image src={cloud} alt="Cloud" />
         <Heading>Lorem ipsum dolor sit amet.</Heading>
         <Paragraph>
@@ -51,7 +104,8 @@ export const HomePart3 = () => {
           minima sequi sunt neque ipsum ipsam repudiandae, nisi quod itaque!
         </Paragraph>
       </Card>
-      <Card style={{ marginTop: "10rem" }}>
+
+      <Card>
         <Image src={cloud} alt="Cloud" />
         <Heading>Lorem ipsum dolor sit amet.</Heading>
         <Paragraph>
